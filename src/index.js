d3cc11e52aac8b4120772a98e0ae23514b8a400c
Praveen Reddy <praveenreddy.peto@gmail.com>
2019-10-15 09:19:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import {createFeatureHub} from '@feature-hub/core';
+import {defineExternals, loadAmdModule} from '@feature-hub/module-loader-amd';
+import {FeatureAppLoader, FeatureHubContextProvider} from '@feature-hub/react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const {featureAppManager} = createFeatureHub('test:todomvc-integrator', {
+  moduleLoader: loadAmdModule,
+  providedExternals: {
+    react: "16.7.0"
+  }
+});
+
+defineExternals({react: React});
+
+ReactDOM.render(
+  <FeatureHubContextProvider value={{featureAppManager}}>
+    <section className="integrator">
+      <FeatureAppLoader
+        featureAppId="consumer:index"
+        src="feature-app-consumer.umd.js"
+      />
+    </section>
+  </FeatureHubContextProvider>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: ['css-loader']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: ['url-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/integrator',
+    publicPath: '/',
+    filename: 'feature-hub-integrator.js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './integrator',
+    hot: true
+  }
+};
